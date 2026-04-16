78943d1099e7186ca807fe75f6d481e508c6d3b0
vandermesis <v@vandermesis.com>
2026-04-16 22:47:07 +0200

    Initial Commit

---
+++ OtiosumTests/OtiosumTests.swift
@@ -0,0 +1,19 @@
+//
+//  OtiosumTests.swift
+//  OtiosumTests
+//
+//  Created by Marek Skrzelowski on 16/04/2026.
+//
+
+import Testing
+@testable import Otiosum
+
+struct OtiosumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
